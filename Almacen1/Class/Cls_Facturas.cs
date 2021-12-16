240077292e19851acef582e9f6aabc7c7625b606
NoanHC <noanhc77@gmail.com>
2021-12-16 09:25:19 -0700

    add

---
+++ Almacen1/Class/Cls_Facturas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Almacen1.Class
+{
+    class Cls_Facturas
+    {
+        ClsMethod method = new ClsMethod();
+        string table = "tb_facturas";
+        string query = "";
+
+        public bool _set(string nombre, string matricula, string direccion, string celular)
+        {
+            string campos = "nombre, matricula, direccion, celular";
+            string values = "'" + nombre + "','" + matricula + "','" + direccion + "','" + celular + "'";
+            return method.set(table, campos, values);
+        }
+        public bool _update(string nombre, string matricula, string direccion, string celular, string id)
+        {
+            string set = "nombre='" + nombre + "', matricula='" + matricula + "', direccion='" + direccion + "', celular='" + celular + "'";
+            return method.update(table, set, "id_estudiante", id);
+        }
+        public void _get(DataGridView dgv)
+        {
+            query = "SELECT id_estudiante as 'Id', nombre as 'Nombre', matricula as 'Matricula', direccion as 'Direccion', celular as 'Celular' FROM tb_estudiante";
+            method.ExecuteQuery(query, dgv);
+        }
+        public void _consult(DataTable dt, string id)
+        {
+            query = "SELECT id_estudiante, nombre, matricula, direccion, celular FROM " + table + " WHERE id_estudiante='" + id + "'";
+            method.Consultar(query, dt);
+        }
+        public bool _delete(string id)
+        {
+            return method.delete(table, "id_estudiante", id);
+        }
+    }
+}
--- Almacen1/Login.Designer.cs
+++ Almacen1/Login.Designer.cs
@@ -42,7 +42,7 @@ namespace Almacen1
             // 
             this.txt_pass.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txt_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_pass.Location = new System.Drawing.Point(64, 159);
+            this.txt_pass.Location = new System.Drawing.Point(124, 241);
             this.txt_pass.MaxLength = 15;
             this.txt_pass.Multiline = true;
             this.txt_pass.Name = "txt_pass";
@@ -55,7 +55,7 @@ namespace Almacen1
             // 
             this.txt_user.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txt_user.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_user.Location = new System.Drawing.Point(64, 98);
+            this.txt_user.Location = new System.Drawing.Point(124, 180);
             this.txt_user.MaxLength = 15;
             this.txt_user.Multiline = true;
             this.txt_user.Name = "txt_user";
@@ -66,7 +66,7 @@ namespace Almacen1
             // label8
             // 
             this.label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label8.Location = new System.Drawing.Point(56, 126);
+            this.label8.Location = new System.Drawing.Point(116, 208);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(240, 2);
             this.label8.TabIndex = 21;
@@ -77,7 +77,7 @@ namespace Almacen1
             this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_login.ForeColor = System.Drawing.Color.White;
-            this.btn_login.Location = new System.Drawing.Point(112, 205);
+            this.btn_login.Location = new System.Drawing.Point(172, 287);
             this.btn_login.Name = "btn_login";
             this.btn_login.Size = new System.Drawing.Size(128, 38);
             this.btn_login.TabIndex = 25;
@@ -90,7 +90,7 @@ namespace Almacen1
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label2.Location = new System.Drawing.Point(61, 140);
+            this.label2.Location = new System.Drawing.Point(121, 222);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(80, 16);
             this.label2.TabIndex = 20;
@@ -101,7 +101,7 @@ namespace Almacen1
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label1.Location = new System.Drawing.Point(61, 79);
+            this.label1.Location = new System.Drawing.Point(121, 161);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 16);
             this.label1.TabIndex = 19;
@@ -110,7 +110,7 @@ namespace Almacen1
             // label3
             // 
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label3.Location = new System.Drawing.Point(56, 188);
+            this.label3.Location = new System.Drawing.Point(116, 270);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(240, 2);
             this.label3.TabIndex = 22;
@@ -119,7 +119,7 @@ namespace Almacen1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(347, 291);
+            this.ClientSize = new System.Drawing.Size(453, 358);
             this.Controls.Add(this.txt_pass);
             this.Controls.Add(this.txt_user);
             this.Controls.Add(this.label8);
