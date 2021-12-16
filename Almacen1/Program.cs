a414e2e97dd8237e5cd13e0f4512a7b8b2ffba81
NoanHC <noanhc77@gmail.com>
2021-12-16 11:13:54 -0700

    adfd

--- Almacen1/Program.cs
+++ Almacen1/Program.cs
@@ -16,7 +16,7 @@ namespace Almacen1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Usuarios.FrmAlta());
+            Application.Run(new Login());
         }
     }
 }
