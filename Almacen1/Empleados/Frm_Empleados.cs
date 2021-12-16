b283e98513ac337fca87636570a866f02f203b4d
GabrielES89 <GabrielEBV89@gmail.com>
2021-12-16 11:52:49 -0700

    Add empleado

---
+++ Almacen1/Empleados/Frm_Empleados.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Almacen1.Empleados
+{
+    public partial class Frm_Empleados : Form
+    {
+        // Clases
+        Class.Cls_Empleados ObjEmpleados = new Class.Cls_Empleados();
+        // Datatables
+        DataTable dt = new DataTable();
+
+        public Frm_Empleados()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAñadir_Click(object sender, EventArgs e)
+        {
+            ObjEmpleados._set(txtNombre.Text, txtTelefono.Text, txtCorreo.Text, txtDireccion.Text, txtPuesto.Text, dt.Rows[cbEstatus.SelectedIndex][0].ToString(), txtMatricula.Text);
+        }
+
+        private void Frm_Empleados_Load(object sender, EventArgs e)
+        {
+            Status();
+        }
+        void Status()
+        {
+            ObjEmpleados._consult_status(dt);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                cbEstatus.Items.Add(dt.Rows[i][1].ToString());
+            }
+        }
+
+    }
+}
